f07346ced34df7a5eadae2bc098b7fd58a3b80b7
kierandidi <kieran.didi@gmail.com>
2022-07-31 20:05:32 +1000

    Finished CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Article = require('./models/article');
 
 mongoose.connect('mongodb://localhost:27017/article-database');
@@ -18,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //tell express to parse bodies as URL encoded of post requests for adding new articles
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -44,7 +45,22 @@ app.get('/articles/:id', async (req, res) => {
     res.render('articles/show', { article });
 });
 
+app.get('/articles/:id/edit', async (req, res) => {
+    const article = await Article.findById(req.params.id);
+    res.render('articles/edit', { article });
+})
 
+app.put('/articles/:id', async (req, res) => {
+    const { id } = req.params;
+    const article = await Article.findByIdAndUpdate(id, {...req.body.article});
+    res.redirect(`articles/${article._id}`);
+});
+
+app.delete('/articles/:id', async (req, res) => {
+    const { id } = req.params;
+    await Article.findByIdAndDelete(id);
+    res.redirect('/articles');
+});
 
 //only test route to see if we can connect express to mongodb
 // app.get('/makearticle', async (req, res) => {
