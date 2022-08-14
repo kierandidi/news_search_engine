290c7d1ae84759a892a8aadddcaf70b7daef2697
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2022-08-14 18:00:38 +0200

    changed logo

--- news-search/src/components/Navbar.js
+++ news-search/src/components/Navbar.js
@@ -1,18 +1,24 @@
-import React from 'react';
-
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from './img/athenas-delta-logo/athenas_delta_logo_transparent.png';
+import Logo2 from './img/athenas-delta-logo/athenas-logo-only.png';
 
 function NavbarExpand() {
+  const [over, setOver] = useState(false);
   return (
     <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to='/'>NewsSearch N-GIN</Navbar.Brand>
+        <Navbar.Brand onMouseOver={() => setOver(true)} onMouseOut={() => setOver(false)}>
+          <Link to='/'>
+            <img width='70px' height='auto' src={over ? Logo : Logo2} alt="Athenas Delta Logo" />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
@@ -20,7 +26,7 @@ function NavbarExpand() {
             <Nav.Link as={Link} to='/'>About</Nav.Link>
             <NavDropdown title="Topics" id="collasible-nav-dropdown">
               <NavDropdown.Item as={Link} to='/global-warming'>Global Warming</NavDropdown.Item>
-              
+
               <NavDropdown.Item as={Link} to='/bitcoin'>
                 Bitcoin
               </NavDropdown.Item>
@@ -33,14 +39,14 @@ function NavbarExpand() {
           <Nav>
             <Nav.Link href="#contact">Contact</Nav.Link>
             <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -49,4 +55,4 @@ function NavbarExpand() {
 }
 
 
-export default NavbarExpand ;
+export default NavbarExpand;
--- news-search/src/components/NewsCard.js
+++ news-search/src/components/NewsCard.js
@@ -4,19 +4,19 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
-const NewsCard = ({date, topic, headline, img, url}) => {
+const NewsCard = ({ date, topic, headline, img, url }) => {
   return (
-    <Card>
+    <Card style={{ marginTop: "20px" }}>
       <Card.Img variant="top" style={{ maxWidth: '634px', maxHeight: '286px' }} src={img} />
       <Card.Body>
-      <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
         <Card.Title>{topic}</Card.Title>
         <Card.Text>
-            {headline}
+          {headline}
         </Card.Text>
         <Button href={url} variant="primary">Read more</Button>
       </Card.Body>
-    </Card>
+    </Card >
   );
 }
 
